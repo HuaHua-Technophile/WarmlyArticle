1aae03b484093e1cbbe17d22ada30021be2aab4f
V-I-G <3118590779@qq.com>
2023-07-10 11:09:43 +0800

    init

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//---------------------bootstrap---------------------------
+import "bootstrap/scss/bootstrap.scss"; //优先引入bootstrap入口文件，使其可以被后续自定义样式与自定义主题覆盖
 import App from './App'
 
 // #ifndef VUE3
@@ -12,7 +14,9 @@ app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
   return {
