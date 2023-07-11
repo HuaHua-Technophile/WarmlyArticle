300d2e9682cf302373843051b3999ce9decdaa3f
V-I-G <3118590779@qq.com>
2023-07-11 10:24:53 +0800

    去除顶栏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <view class="content">
-    <view class="text-danger">test bootstrap</view>
+  <view class="vw-100 vh-100 d-flex justify-content-center align-items-center bg-body-secondary">
+    <view class="text-danger">{{title}}</view>
   </view>
 </template>
 
@@ -8,7 +8,7 @@
   export default {
     data() {
       return {
-        title: 'Hello'
+        title: 'Hello2'
       }
     },
     onLoad() {
@@ -21,29 +21,4 @@
 </script>
 
 <style>
-  .content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin-top: 200rpx;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 50rpx;
-  }
-
-  .text-area {
-    display: flex;
-    justify-content: center;
-  }
-
-  .title {
-    font-size: 36rpx;
-    color: #8f8f94;
-  }
 </style>
