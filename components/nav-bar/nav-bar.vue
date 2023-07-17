c77a07c56f358d5db2932b577f2919e1bdc84829
V-I-G <3118590779@qq.com>
2023-07-17 17:20:31 +0800

    1

--- components/nav-bar/nav-bar.vue
+++ components/nav-bar/nav-bar.vue
@@ -1,17 +1,30 @@
 <template>
-  <view id="navBar" class="d-flex justify-content-between position-fixed bottom-0 vw-100 bg-dark bg-opacity-10"
+  <view
+    id="navBar"
+    class="d-flex justify-content-between position-fixed bottom-0 vw-100 bg-dark bg-opacity-10"
     style="height: 50px; color: #7c817b !important">
-    <view class="h-100 overflow-hidden d-flex justify-content-center align-items-center position-relative" id="navBar0"
-      data-id="0" style="width: 36%" @click="addRipple">
+    <view
+      class="h-100 overflow-hidden d-flex justify-content-center align-items-center position-relative"
+      id="navBar0"
+      data-id="0"
+      style="width: 36%"
+      @click="addRipple">
       首页
     </view>
     <view
       class="h-100 overflow-hidden d-flex justify-content-center align-items-center position-absolute top-50 start-50 translate-middle"
-      style="width: 36%" id="navBar1" data-id="1" @click="addRipple">
+      style="width: 36%"
+      id="navBar1"
+      data-id="1"
+      @click="addRipple">
       探索
     </view>
-    <view class="h-100 overflow-hidden d-flex justify-content-center align-items-center position-relative"
-      style="width: 36%" id="navBar2" data-id="2" @click="addRipple">
+    <view
+      class="h-100 overflow-hidden d-flex justify-content-center align-items-center position-relative"
+      style="width: 36%"
+      id="navBar2"
+      data-id="2"
+      @click="addRipple">
       我
     </view>
   </view>
@@ -27,10 +40,9 @@
       position:absolute;
       bottom:calc(100vh - ${e.detail.y}px);
       left:calc(${e.detail.x}px - ${32.08 * e.currentTarget.dataset.id}vw);
-      animation: ripple 2s both !important;
-`
+      animation: ripple 2s both !important;`
     );
-    ripple.classList.add('ripple')
+    ripple.classList.add("ripple");
     let target = document.getElementById(`navBar${e.currentTarget.dataset.id}`);
     target.appendChild(ripple);
     setTimeout(() => {
@@ -39,6 +51,4 @@
   };
 </script>
 
-<style>
-
-</style>
+<style></style>
